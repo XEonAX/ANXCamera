f2f0da665f16cb15210f778a9436346a1100ade3
RomanTheLegend <roman@upgration.com>
2020-12-29 03:22:06 +0100

    Enforce aapt2 for recompilation

--- recompile.bat
+++ recompile.bat
@@ -1,1 +1,1 @@
-java  -jar ..\ANXMiuiPortTools\apktool.jar b -p ..\ANXMiuiPortTools\MiuiFrameworks -o .\out\ANXCamera-Unsigned.apk .\src\ANXCamera
+java  -jar ..\ANXMiuiPortTools\apktool.jar b --use-aapt2 -p ..\ANXMiuiPortTools\MiuiFrameworks -o .\out\ANXCamera-Unsigned.apk .\src\ANXCamera
